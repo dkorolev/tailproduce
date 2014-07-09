40f76e65693de88f7be915a17c06bd2481820b29
Brian Francois <brian@staance.com>
2014-07-09 12:02:09 -0700

    Remove redundantly declared Stream object

--- src/streams_registry.h
+++ src/streams_registry.h
@@ -34,16 +34,6 @@ namespace TailProduce {
             streams.push_back(StreamsRegistryEntry{impl, name, entry_type, order_key_type});
         }
     };
-
-    struct Stream {
-        Stream(StreamsRegistry& registry,
-               const std::string& stream_name,
-               const std::string& entry_type_name,
-               const std::string& order_key_type_name) {
-            registry.Add(this, stream_name, entry_type_name, order_key_type_name);
-        }
-    };
-
 };
 
 #endif
