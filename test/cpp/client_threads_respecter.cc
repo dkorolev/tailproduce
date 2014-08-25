31205f30e871329210a58691e814af26aa67dd87
Dima <dmitry.korolev@gmail.com>
2014-08-25 12:00:53 -0700

    Added more tear down tests.

--- test/cpp/client_threads_respecter.cc
+++ test/cpp/client_threads_respecter.cc
@@ -18,21 +18,28 @@ TEST(ClientThreadsRespecter, Smoke) {
         CTR ctr;
 
         std::thread([&ctr, &x, &xb]() {
-                        auto client = ctr.RegisterScopedClient();
-                        while (client) {
-                            VLOG(2) << "++x";
-                            ++x;
-                            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+                        {
+                            auto client = ctr.RegisterScopedClient();
+                            while (client) {
+                                VLOG(2) << "++x";
+                                ++x;
+                                std::this_thread::sleep_for(std::chrono::milliseconds(10));
+                            }
+                            VLOG(2) << "Terminating `++x` in 0.1s.";
+                            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                            xb = true;
+                            VLOG(2) << "Terminated `++x`.";
                         }
-                        VLOG(2) << "Terminating `++x` in 0.1s.";
-                        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-                        xb = true;
-                        VLOG(2) << "Terminated `++x`.";
+                        VLOG(2) << "The `++x` thread is done, ensuring that spawning another client would not work.";
+                        ASSERT_THROW(auto client = ctr.RegisterScopedClient(),
+                                     ::TailProduce::AlreadyInTearDownModeException);
+                        VLOG(2) << "The `++x` thread is done, confirmed that spawning another client does not work.";
                     }).detach();
 
         std::thread([&ctr, &y, &yb]() {
                         std::function<void(const CTR::Client&)> lambda;
-                        ctr.RunClientCode(lambda = [&y, &yb](const CTR::Client& client) {
+                        VLOG(2) << "Running the `++y` thread.";
+                        EXPECT_TRUE(ctr.RunClientCode(lambda = [&y, &yb](const CTR::Client& client) {
                             while (client) {
                                 VLOG(2) << "++y";
                                 ++y;
@@ -42,7 +49,11 @@ TEST(ClientThreadsRespecter, Smoke) {
                             std::this_thread::sleep_for(std::chrono::milliseconds(100));
                             yb = true;
                             VLOG(2) << "Terminated `++y`.";
-                        });
+                        }));
+                        VLOG(2) << "The `++y` thread is done, ensuring that spawning another client would not work.";
+                        EXPECT_FALSE(
+                            ctr.RunClientCode(lambda = [](const CTR::Client& client) { ASSERT_TRUE(false); }));
+                        VLOG(2) << "The `++y` thread is done, confirmed that spawning another client does not work.";
                     }).detach();
 
         VLOG(2) << "Let `++x` and `++y` threads run for 0.25s.";
