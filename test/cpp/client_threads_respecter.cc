96fe4b792bfd2863460bcb51046a570ab2506dd6
Dima <dmitry.korolev@gmail.com>
2014-08-25 16:11:12 -0700

    And another minor touch to comments and construct.

--- test/cpp/client_threads_respecter.cc
+++ test/cpp/client_threads_respecter.cc
@@ -18,6 +18,7 @@ TEST(ClientThreadsRespecter, Smoke) {
         CTR ctr;
 
         std::thread([&ctr, &x, &xb]() {
+                        // `top_level_client` is necessary to keep the CTR alive after the 1st inner client is done.
                         auto top_level_client = ctr.RegisterScopedClient();
                         {
                             auto client = ctr.RegisterScopedClient();
@@ -37,26 +38,29 @@ TEST(ClientThreadsRespecter, Smoke) {
                         VLOG(2) << "The `++x` thread is done, confirmed that spawning another client does not work.";
                     }).detach();
 
-        std::thread([&ctr, &y, &yb]() {
-                        auto top_level_client = ctr.RegisterScopedClient();
-                        std::function<void(const CTR::Client&)> lambda;
-                        VLOG(2) << "Running the `++y` thread.";
-                        EXPECT_TRUE(ctr.RunClientCode(lambda = [&y, &yb](const CTR::Client& client) {
-                            while (client) {
-                                VLOG(2) << "++y";
-                                ++y;
-                                std::this_thread::sleep_for(std::chrono::milliseconds(10));
-                            }
-                            VLOG(2) << "Terminating `++y` in 0.1s.";
-                            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-                            yb = true;
-                            VLOG(2) << "Terminated `++y`.";
-                        }));
-                        VLOG(2) << "The `++y` thread is done, ensuring that spawning another client would not work.";
-                        EXPECT_FALSE(
-                            ctr.RunClientCode(lambda = [](const CTR::Client& client) { ASSERT_TRUE(false); }));
-                        VLOG(2) << "The `++y` thread is done, confirmed that spawning another client does not work.";
-                    }).detach();
+        std::thread(
+            [&ctr, &y, &yb]() {
+                std::function<void(const CTR::Client&)> top_level_lambda;
+                // `top_level_lambda` is necessary to keep the CTR alive after the 1st inner client is done.
+                EXPECT_TRUE(ctr.RunClientCode(top_level_lambda = [&ctr, &y, &yb](const CTR::Client&) {
+                    std::function<void(const CTR::Client&)> lambda;
+                    VLOG(2) << "Running the `++y` thread.";
+                    EXPECT_TRUE(ctr.RunClientCode(lambda = [&y, &yb](const CTR::Client& client) {
+                        while (client) {
+                            VLOG(2) << "++y";
+                            ++y;
+                            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+                        }
+                        VLOG(2) << "Terminating `++y` in 0.1s.";
+                        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                        yb = true;
+                        VLOG(2) << "Terminated `++y`.";
+                    }));
+                    VLOG(2) << "The `++y` thread is done, ensuring that spawning another client would not work.";
+                    EXPECT_FALSE(ctr.RunClientCode(lambda = [](const CTR::Client& client) { ASSERT_TRUE(false); }));
+                    VLOG(2) << "The `++y` thread is done, confirmed that spawning another client does not work.";
+                }));
+            }).detach();
 
         VLOG(2) << "Let `++x` and `++y` threads run for 0.25s.";
         std::this_thread::sleep_for(std::chrono::milliseconds(250));
